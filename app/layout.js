ddc9dc0e8f64e94fe641c1474255ad0f2c76424c
aditya <adityasoni53@gmail.com>
2024-05-22 17:44:59 +0530

    Added SpeedInsights

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import CommonLayout from "@/components/client-view/common-layout";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <CommonLayout>{children}</CommonLayout>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
